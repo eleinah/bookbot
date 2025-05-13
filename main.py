a49e119e8fc4ef705ce79216a45c1de554643e09
Ellie <ellie@lly.dev>
2025-05-13 16:36:48 -0500

    Added reporting to main()

--- main.py
+++ main.py
@@ -3,7 +3,16 @@
 import stats
 
 def main():
-    print(stats.get_char_count("frankenstein"))
+    book = "frankenstein"
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/{book}.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {stats.get_word_count(book)} total words")
+    print("--------- Character Count -------")
+    sort_list = stats.sort_chars(book)
+    for dicts in sort_list:
+        print(f"{dicts['char']}: {dicts['num']}")
+    print("============= END ===============")
 
 if __name__ == "__main__":
     main()
