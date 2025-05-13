aae390be8598fff734dc4a776c0f1f79609ecd42
Ellie <ellie@lly.dev>
2025-05-13 03:47:34 -0500

    moved read func to stats.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 from stats import get_word_count
 
-def get_book_text(book):
-    with open(f"books/{book}.txt") as b:
-        contents = b.read()
-    return contents
-
 def main():
     print(get_word_count("frankenstein"))
 
--- stats.py
+++ stats.py
@@ -1,4 +1,7 @@
-from main import get_book_text
+def get_book_text(book):
+    with open(f"books/{book}.txt") as b:
+        contents = b.read()
+    return contents
 
 def get_word_count(book):
     words = get_book_text(book).split()
