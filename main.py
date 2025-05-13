bacd43534157712597a3b3c7fc7941a9da2364dd
Ellie <ellie@lly.dev>
2025-05-13 14:09:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from stats import get_word_count
+#!/usr/bin/env python3
+
+from stats import get_char_count
 
 def main():
-    print(get_word_count("frankenstein"))
+    print(get_char_count("frankenstein"))
 
 if __name__ == "__main__":
     main()
